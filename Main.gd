217c7ff63419eaaf1669d9f191f754a625a77713
Kevin Oh <aflashyrhetoric@gmail.com>
2022-01-19 02:06:44 -0500

    Update script to use init function with instance variables

--- Main.gd
+++ Main.gd
@@ -16,23 +16,48 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("hit_x"):
-		var children = get_tree().get_nodes_in_group("shapes_group")
+		var children = get_tree().get_nodes_in_group("x_sprite")
 		print(children)
 		if children.size() > 0:
 			var current_child = children.pop_back()
 			current_child.queue_free()
 			score += 1
 			$Score.text = str(score)
-	pass
+	if Input.is_action_just_pressed("hit_o"):
+		var children = get_tree().get_nodes_in_group("o_sprite")
+		print(children)
+		if children.size() > 0:
+			var current_child = children.pop_back()
+			current_child.queue_free()
+			score += 1
+			$Score.text = str(score)
+	if Input.is_action_just_pressed("hit_t"):
+		var children = get_tree().get_nodes_in_group("triangle_sprite")
+		print(children)
+		if children.size() > 0:
+			var current_child = children.pop_back()
+			current_child.queue_free()
+			score += 1
+			$Score.text = str(score)
+	if Input.is_action_just_pressed("hit_s"):
+		var children = get_tree().get_nodes_in_group("square_sprite")
+		print(children)
+		if children.size() > 0:
+			var current_child = children.pop_back()
+			current_child.queue_free()
+			score += 1
+			$Score.text = str(score)
+	pass 
 
 func _on_X_spawn_shape():
 	var x_force = rng.randf_range(-75.0, 75.0)
 	var y_force = rng.randf_range(-250.0, -300.0)
-	var new_vel = Vector2(x_force, y_force)
+	var new_vel = Vector2(x_force,	 y_force)
 	var shape = X.instance()
 	shape.position = spawn_position
 	shape.linear_velocity = new_vel
-	shape.add_to_group("shapes_group")
+	shape.add_to_group(shape.shape_to_show)
+	print("shape to show: ", shape.get_current_shape())
 	$SpawnPop.play()
 	add_child(shape)
 	pass
--- src/Shapes/ShapeX.gd
+++ src/Shapes/ShapeX.gd
@@ -2,22 +2,26 @@ extends RigidBody2D
 
 signal spawn_shape
 
+var shape_to_show = ""
+
+func _init():
+	randomize()
+	shape_to_show = get_random_shape() + "_sprite"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	randomize()
-	
-	var s = get_shape() + "_sprite"
-	var shape_to_show = get_node(s)
-	print("Print new shape", " ", s)
-	shape_to_show.visible = true
+	var shape = get_node(shape_to_show)
+	shape.visible = true
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-func get_shape():
+func get_current_shape():
+	return shape_to_show
+
+func get_random_shape():
 	var _shapes = ["o", "x", "square", "triangle"]
 	var random_shape = _shapes[randi() % _shapes.size()]
 	return random_shape
@@ -25,4 +29,3 @@ func get_shape():
 func _on_ShapeTimer_timeout():
 	emit_signal("spawn_shape")
 	pass
-
