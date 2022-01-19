2e0cafdb5e5e0b36a2be3560dee2f16f587b3f57
Kevin Oh <aflashyrhetoric@gmail.com>
2022-01-19 00:49:37 -0500

    Add shapes, randomize shapes that spawn

--- src/Shapes/ShapeX.gd
+++ src/Shapes/ShapeX.gd
@@ -2,16 +2,27 @@ extends RigidBody2D
 
 signal spawn_shape
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
+	
+	var s = get_shape() + "_sprite"
+	var shape_to_show = get_node(s)
+	print("Print new shape", " ", s)
+	shape_to_show.visible = true
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func get_shape():
+	var _shapes = ["o", "x", "square", "triangle"]
+	var random_shape = _shapes[randi() % _shapes.size()]
+	return random_shape
+
 func _on_ShapeTimer_timeout():
-	print("Print new shape")
 	emit_signal("spawn_shape")
 	pass
 
